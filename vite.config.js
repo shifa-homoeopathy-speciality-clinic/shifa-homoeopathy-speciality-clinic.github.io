b665a67f224d010f77a5041e5e9f6cd8b05aadd8
hisham <hisham@nalashaa.com>
2025-08-26 15:56:08 +0530

    Changed the base to the new repository

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/shifa-homoeopathy-speciality-clinic/"
+  base: "/shifa-homoeopathy-speciality-clinic.github.io/"
 })
